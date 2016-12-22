2c13f12ccc3e33a3949834d3d8749c23c19326b4
cmhnk <corinne@automatedinsights.com>
2016-12-22 17:42:11 -0500

    Write tests; create About page manually

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
+    # renders the View corresponding to this action
+    # in this case, home.html.erb
   end
 
   def help
+  end
+
+  def about
   end
 end
